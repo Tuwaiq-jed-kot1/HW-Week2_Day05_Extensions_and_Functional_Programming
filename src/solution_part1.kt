342c7953f2a23ea8ea12c15f8801f5a3c8fdc09b
NadiaAliAlmrashi <Nadia000333666>
2021-10-08 17:20:49 +0300

    good Homework

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,6 +1,6 @@
 fun main() {
 ////part I//////
-    "NAAadia Ali;".repetativeStr()
+    "NAAadia Ali:".repetativeStr()
 
 ////part II//////
 
@@ -12,14 +12,14 @@ fun String.repetativeStr() {
     var count = 0
     var str1: String = ""
     var str = this.toMutableList()
-    while (count < str.size - 1) {
+    while (count < str.size-1 ) {
 
         if (str[count] == str[count + 1]) {
             str[count] = '*'
 
         }
         str1 = str1 + str[count]
-        count += 1
+        count ++
 
     }
 
